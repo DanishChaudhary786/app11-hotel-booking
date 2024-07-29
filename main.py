8dba25a1edab7681437bdb7c1205643501f847e8
DC <chaudharydanish024@gmail.com>
2024-07-29 16:26:25 +0530

    Inheritance

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import self
 
 df = pd.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+df_cards_security = pd.read_csv("card_security.csv", dtype=str)
 
 
 class Hotel:
@@ -24,6 +26,13 @@ class Hotel:
             return False
 
 
+class SpaHotel(Hotel):
+
+    def book_spa_hotel(self):
+        pass
+
+
+
 class ReservationTicket:
 
     def __init__(self, customer_name, hotel_object):
@@ -32,21 +41,78 @@ class ReservationTicket:
 
     def generate(self):
         content = f"""
-            Than you for your reservation!
+            Thank you for your reservation!
             Here are your reservation details:
             Name: {self.customer_name}
             Hotel: {self.hotel.name}
             """
         return content
 
+class SpaTicket:
+
+    def __init__(self, customer_name, spa_object):
+        self.customer_name = customer_name
+        self.spa = spa_object
+
+    def generate(self):
+        content = f"""
+            Thank you for your reservation!
+            Here are your reservation details:
+            Name: {self.customer_name}
+            Spa: {self.spa.name}
+            """
+        return content
+class CreditCard:
+    def __init__(self, card_number):
+        self.card_number = card_number
+        # self.expiration = expiration
+        # self.security_code = security_code
+        # self.holder_name = holder_name
+
+    def validate(self, expiration, security_code, holder_name):
+        card_data = {"number": self.card_number, "expiration": expiration,
+                     "cvc": security_code, "holder": holder_name}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+class SecureCreditCard(CreditCard):
+
+    def auhenticate(self, given_password):
+        password = df_cards_security.loc[df_cards_security["number"] == self.card_number, "password"].squeeze()
+        if password == given_password:
+            return True
+
 
 print(df)
 hotel_id = input("Enter the id of the hotel: ")
-hotel = Hotel(hotel_id=hotel_id)
+hotel = SpaHotel(hotel_id=hotel_id)
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservationTicket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservationTicket.generate())
+    card_number = input("Enter your card number: ")
+    expiration = input("Enter expiration date: ")
+    security_code = input("Enter security code: ")
+    holder_name = input("Enter holder name: ")
+    credit_card = SecureCreditCard(card_number=card_number)
+    if credit_card.validate(expiration=expiration, security_code=security_code, holder_name=holder_name):
+        password = input("Enter your password: ")
+        if credit_card.auhenticate(given_password=password):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservationTicket = ReservationTicket(customer_name=name, hotel_object=hotel)
+            print(reservationTicket.generate())
+            spa_package = input("Do you want spa package? (yes/no): ")
+            if spa_package.lower() == "yes":
+                hotel.book_spa_hotel()
+                spa_ticket = SpaTicket(
+                    customer_name=name,
+                    spa_object=hotel
+                )
+                print(spa_ticket.generate())
+        else:
+            print("Credit card authentication failed. Please try again.")
+    else:
+        print("There was an error with your credit card. Please try again.")
 else:
     print("Hotel is not free.")
